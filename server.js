50cc347432620d90af8837fd70bbcaa884105f8c
root <davidw93.dw@gmail.com>
2014-07-02 14:38:49 +0000

    updates

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var mongoose    = require("mongoose");
+/*var mongoose    = require("mongoose");
 
 global.framework= require("./system/framework");
 var config      = require("./server/config"); //load the server-only config files
@@ -15,4 +15,14 @@ var db          = mongoose.connect(config.database.Development.db, function(err)
 
     console.log("Server now listening on " + config.server.port);
 });
+*/
 
+var net = require("net");
+var mqtt = require("./mqtt");
+
+var client = new mqtt.MQTTClient(9000, "127.0.0.1", "david");
+client.addListener("mqttData", function(topic, payload) {
+    console.log(topic + " == " + payload);
+});
+
+client.subscribe("/demo/channel");
